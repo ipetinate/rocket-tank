93dd29e8672ed15e2e235cc0d97da12998df5520
Isac Petinate <idpetinate@gmail.com>
2023-12-23 09:51:02 -0300

    feat: Implementing basic code to move forward and backward

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,59 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// PINS H Bridge L298n
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+const int8_t PIN_H_BRIDGE_A = 11;
+const int8_t PIN_H_BRIDGE_B = 10;
+const int8_t PIN_H_BRIDGE_C = 6;
+const int8_t PIN_H_BRIDGE_D = 5;
+
+// Methods
+void moveForwardStart();
+void moveForwardStop();
+
+void moveBackwardStart();
+void moveBackwardStop();
+
+void setup()
+{
+  pinMode(PIN_H_BRIDGE_A, OUTPUT);
+  pinMode(PIN_H_BRIDGE_B, OUTPUT);
+  pinMode(PIN_H_BRIDGE_C, OUTPUT);
+  pinMode(PIN_H_BRIDGE_D, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  moveForwardStart();
+  delay(2000);
+  moveForwardStop();
+  moveBackwardStart();
+  delay(2000);
+  moveBackwardStop();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// Method's implementation
+
+void moveForwardStart()
+{
+  digitalWrite(PIN_H_BRIDGE_B, HIGH);
+  digitalWrite(PIN_H_BRIDGE_C, HIGH);
+};
+
+void moveForwardStop()
+{
+  digitalWrite(PIN_H_BRIDGE_B, LOW);
+  digitalWrite(PIN_H_BRIDGE_C, LOW);
+};
+
+void moveBackwardStart()
+{
+  digitalWrite(PIN_H_BRIDGE_A, HIGH);
+  digitalWrite(PIN_H_BRIDGE_D, HIGH);
+};
+
+void moveBackwardStop()
+{
+  digitalWrite(PIN_H_BRIDGE_A, LOW);
+  digitalWrite(PIN_H_BRIDGE_D, LOW);
+};
